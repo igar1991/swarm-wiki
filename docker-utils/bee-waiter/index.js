4c3faecdb6b2419c929c3e7d0f4349bbed6b9272
igar1991 <iiggaarr19912004@gmail.com>
2022-07-27 12:34:42 +0300

    upgraded bee waiter

--- docker-utils/bee-waiter/index.js
+++ docker-utils/bee-waiter/index.js
@@ -1,28 +1,26 @@
 import fetch from 'node-fetch';
 import {sleep} from '../utils/utils.js';
 
-const beeDebugUrl = process.env.WIKI_BEE_DEBUG_URL;
+const beeUrl = process.env.WIKI_BEE_URL;
 const triggerUrl = process.env.WIKI_TRIGGER_URL;
-
-if (!beeDebugUrl) {
-    throw new Error('WIKI_BEE_DEBUG_URL is not set');
-}
 
 if (!triggerUrl) {
     throw new Error('WIKI_TRIGGER_URL is not set');
 }
 
+if (!beeUrl) {
+    throw new Error('WIKI_BEE_URL is not set');
+}
+
 (async function () {
     while (true) {
         try {
-            const json = await (await fetch(`${beeDebugUrl}readiness`)).json();
+            const json = await (await fetch(`${beeUrl}feeds/fffffA46f2883920e6f4976CF2F2E905523d80E6/00fbf04e4619b79da2e2ca5f4dae35cf00d622bce6fbe755f577baf500e7bd11?type=sequence`)).json();
             console.log(json)
-            if (json.status === 'ok') {
-                await fetch(triggerUrl + 'run', {
-                    method: 'POST',
-                });
-                return
-            }
+            await fetch(triggerUrl + 'run', {
+                method: 'POST',
+            });
+            return
         } catch (e) {
             console.log(e)
         }
